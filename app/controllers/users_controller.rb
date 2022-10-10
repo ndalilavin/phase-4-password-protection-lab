71c06c60ca6cc162c1184ae9958b24bf8e0b1414
ndalilavin <vinson.steve@student.moringaschool.com>
2022-10-10 22:06:17 +0300

    commit tree 98bd2197f45521a1efe22d1546988d93840d7683

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+    def create
+        user = User.create(user_params)
+        session[:user_id]=user.id
+        if user.valid?
+          render json: user, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        return render json: {error: "Not authorized"}, status: :unauthorized unless session.include? :user_id
+        user=User.find_by(id:session[:user_id])
+
+        render json: user, status: :ok 
+    end
+    
+    private
+    
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+end
